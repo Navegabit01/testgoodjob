57cf524dd02ce0f567bdce5a2310bac38ebeeda1
Luis Alberto <luis.alberto@dspot.pl.com>
2023-06-14 22:12:19 -0400

    Feat: Write a function called merge_arrays that accepts two sorted arrays of integers as parameters and returns a single sorted array containing all elements.

---
+++ Excersise1/exercise1.py
@@ -0,0 +1,33 @@
+def merge_arrays(integer_list1: list, integer_list2: list) -> list:
+    """
+    Merge two list of integers and return the sorted list
+    """
+    merge_list: list = []
+    try:
+        merge_list = integer_list1 + integer_list2
+        merge_list = list(map(lambda x: int(x), merge_list))
+        merge_list.sort()
+    except Exception as errors:
+        # Manage any errors maybe in sentry
+        print(errors)
+        raise ValueError(errors)
+    finally:
+        # Manage memory release
+        del integer_list1, integer_list2
+        return merge_list
+
+
+print(merge_arrays([1, 2, 1], [3, 4]))
+
+# Apply tests
+# Test case 1
+assert merge_arrays([1, 1, 2, 5], [3, 4, 5, 7]) == [1, 1, 2, 3, 4, 5, 5, 7]
+
+# Test case 2
+assert merge_arrays([], []) == []
+
+# Test case 3
+assert merge_arrays([1], [1]) == [1, 1]
+
+# Test case 4
+assert merge_arrays([1, 3], [2, 4, 6]) == [1, 2, 3, 4, 6]
