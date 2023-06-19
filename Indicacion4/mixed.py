1eb07a259b0c4bf6b63b28ffab3a7cbe43aa20f9
Luis Alberto <luis.alberto@dspot.pl.com>
2023-06-18 23:15:30 -0400

    Refactor: Use only threads for the numbers of cores of the machine

--- Indicacion4/mixed.py
+++ Indicacion4/mixed.py
@@ -1,32 +1,31 @@
+import math
+import multiprocessing
 import threading
 
 from Excercise8.excercise8 import binary_search_recursive
 
 
-def advanced_search(search_list: list, target_list: list) -> list:
-    try:
-        return list(map(lambda x: binary_search_recursive(search_list, x), target_list))
-    except Exception as e:
-        print(e)
-        raise ValueError(e)
+def advanced_search(search_list: list, target_list: list, result: list) -> None:
+    result.extend(list(map(lambda x: binary_search_recursive(search_list, x), target_list)))
 
 
 def advanced_search_threads(search_list: list, target_list: list) -> list:
+    """
+    Function to use only threads equal to the numbers of cores of the machine to divide the target list
+    """
     try:
-        results = [None] * len(target_list)
-        threads = []
 
-        # Define a function to run in each thread
-        def search_thread(index):
-            results[index] = binary_search_recursive(search_list, target_list[index])
-
-        # Create a thread for each target in the list
-        for i in range(len(target_list)):
-            thread = threading.Thread(target=search_thread, args=(i,))
+        if not search_list or not target_list:
+            return []
+        num_cores = multiprocessing.cpu_count()  # set the number of cores to use
+        sublist_size = math.ceil(len(target_list) / num_cores)  # calculate the size of each sublist
+        sublist = [target_list[i:i + sublist_size] for i in range(0, len(target_list), sublist_size)]
+        threads = []  # create a list to hold the threads
+        results = []
+        for sublist in sublist:
+            thread = threading.Thread(target=advanced_search, args=(search_list, sublist, results))
             threads.append(thread)
             thread.start()
-
-        # Wait for all threads to finish
         for thread in threads:
             thread.join()
 
@@ -37,13 +36,6 @@ def advanced_search_threads(search_list: list, target_list: list) -> list:
         raise ValueError(e)
 
 
-def test_advanced_search():
-    # Unit test empty list
-    assert advanced_search([], []) == []
-
-    # Unit test 2 list
-    assert advanced_search([2, 5, 6], [2, 4, 6, 8]) == [True, False, True, False]
-
 def test_advanced_search_threads():
     # Unit test empty list
     assert advanced_search_threads([], []) == []
@@ -52,4 +44,4 @@ def test_advanced_search_threads():
     assert advanced_search_threads([2, 5, 6], [2, 4, 6, 8]) == [True, False, True, False]
 
 
-test_advanced_search()
+test_advanced_search_threads()
